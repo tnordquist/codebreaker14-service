ee21a7ce8100fed5190172b4c548dda379cd996f
Todd Nordquist <toddnordquist144@gmail.com>
2021-11-10 16:51:06 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/CodebreakerApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebreakerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodebreakerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/CodebreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebreakerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
