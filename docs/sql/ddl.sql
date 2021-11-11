5c3606d921d01645dc1d561d8230af1d9c3e0c45
Todd Nordquist <toddnordquist144@gmail.com>
2021-11-11 15:03:25 -0700

    Game (nearly) complete, w/ reciprocal relationship

--- docs/sql/ddl.sql
+++ docs/sql/ddl.sql
@@ -1,11 +1,12 @@
 create table game
 (
-    game_id      UUID      not null,
-    created      timestamp not null,
-    external_key UUID      not null,
-    length       integer   not null,
-    pool         varchar(255),
-    text         varchar(255),
+    game_id      UUID         not null,
+    created      timestamp    not null,
+    external_key UUID         not null,
+    length       integer      not null,
+    pool         varchar(255) not null,
+    text         varchar(20)  not null,
+    user_id      UUID         not null,
     primary key (game_id)
 );
 create table user_profile
@@ -17,8 +18,14 @@ create table user_profile
     oauth_key    varchar(30)  not null,
     primary key (user_id)
 );
+alter table game
+    add constraint UK_h5ly13atfey58ueo3eccgihug unique (external_key);
 create index IDXakmwux4w2swsj69pg3ignha1v on user_profile (created);
 alter table user_profile
     add constraint UK_j35xlx80xoi2sb176qdrtoy69 unique (display_name);
 alter table user_profile
+    add constraint UK_22o8v4jg08yk7piojnowil30o unique (external_key);
+alter table user_profile
     add constraint UK_6f815wi5o4jq8p1q1w63o4mhd unique (oauth_key);
+alter table game
+    add constraint FKr01mjyhctq2qcstrix8juol8o foreign key (user_id) references user_profile;
--- src/main/java/edu/cnm/deepdive/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/model/entity/User.java
@@ -1,18 +1,23 @@
 package edu.cnm.deepdive.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import org.hibernate.Incubating;
 import org.hibernate.annotations.CreationTimestamp;
 
+@SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 @Table(
     name = "user_profile",
@@ -27,7 +32,7 @@ public class User {
   @Column(name = "user_id", updatable = false, columnDefinition = "UUID")
   private UUID id;
 
-  @Column(updatable = false, nullable = false, columnDefinition = "UUID")
+  @Column(updatable = false, nullable = false, columnDefinition = "UUID", unique = true)
   private UUID externalKey = UUID.randomUUID();
 
   @CreationTimestamp
@@ -40,6 +45,10 @@ public class User {
 
   @Column(nullable = false, updatable = true, unique = true, length = 100)
   private String displayName;
+
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+  @OrderBy
+  private final List<Game> games = new LinkedList<>();
 
   public UUID getId() {
     return id;
@@ -67,5 +76,9 @@ public class User {
 
   public void setDisplayName(String displayName) {
     this.displayName = displayName;
+  }
+
+  public List<Game> getGames() {
+    return games;
   }
 }
