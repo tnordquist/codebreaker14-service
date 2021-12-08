f4da6a080b7f1deddbcd01ad02b4d1952a3ddc29
Todd Nordquist <toddnordquist144@gmail.com>
2021-12-07 22:12:09 -0700

    User controller done; game controller in progress.

--- src/main/java/edu/cnm/deepdive/model/entity/Game.java
+++ src/main/java/edu/cnm/deepdive/model/entity/Game.java
@@ -1,134 +1,149 @@
 package edu.cnm.deepdive.model.entity;
 
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
-import javax.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
 
 @SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 @Table(
-        indexes = {
-                @Index(columnList = "poolSize"),
-                @Index(columnList = "user_id, created")
-        }
+    indexes = {
+        @Index(columnList = "poolSize"),
+        @Index(columnList = "user_id, created")
+    }
 )
 @JsonInclude(Include.NON_NULL)
 @JsonPropertyOrder({"id", "created", "pool", "length", "solved", "text"})
 public class Game {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "game_id", updatable = false, columnDefinition = "UUID")
-    private UUID id;
+  @Id
+  @GeneratedValue
+  @Column(name = "game_id", updatable = false, columnDefinition = "UUID")
+  @JsonIgnore
+  private UUID id;
 
-    @Column(updatable = false, nullable = false, columnDefinition = "UUID", unique = true)
-    @JsonProperty(value = "id", access = Access.READ_ONLY)
-    private UUID externalKey = UUID.randomUUID();
+  @Column(nullable = false, updatable = false, columnDefinition = "UUID", unique = true)
+  @JsonProperty(value = "id", access = Access.READ_ONLY)
+  private UUID externalKey = UUID.randomUUID();
 
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false, updatable = false)
-    @JsonIgnore
-    private User user;
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
+  @JsonIgnore
+  private User user;
 
-    @CreationTimestamp
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(nullable = false, updatable = false)
-    @JsonProperty(access = Access.READ_ONLY)
-    private Date created;
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  @JsonProperty(access = Access.READ_ONLY)
+  private Date created;
 
-    @Column(nullable = false, updatable = false, length = 255)
-    private String pool;
+  @Column(nullable = false, updatable = false, length = 255)
+  private String pool;
 
-    @Column(nullable = false, updatable = false)
-    @JsonIgnore
-    private int poolSize;
+  @Column(nullable = false, updatable = false)
+  @JsonIgnore
+  private int poolSize;
 
-    @Column(nullable = false, updatable = false)
-    private int length;
+  @Column(nullable = false, updatable = false)
+  private int length;
 
-    @Column(name = "game_text", nullable = false, updatable = false, length = 20)
-    @JsonIgnore
-    private String text;
+  @Column(name = "game_text", nullable = false, updatable = false, length = 20)
+  @JsonIgnore
+  private String text;
 
-    @OneToMany(mappedBy = "game", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    @OrderBy("created ASC")
-    @JsonIgnore
-    private final List<Guess> guesses = new LinkedList<>();
+  @OneToMany(mappedBy = "game", fetch = FetchType.EAGER,
+      cascade = CascadeType.ALL, orphanRemoval = true)
+  @OrderBy("created ASC")
+  @JsonIgnore
+  private final List<Guess> guesses = new LinkedList<>();
 
-    public UUID getId() {
-        return id;
-    }
+  public UUID getId() {
+    return id;
+  }
 
-    public UUID getExternalKey() {
-        return externalKey;
-    }
+  public UUID getExternalKey() {
+    return externalKey;
+  }
 
-    public Date getCreated() {
-        return created;
-    }
+  public Date getCreated() {
+    return created;
+  }
 
-    public User getUser() {
-        return user;
-    }
+  public User getUser() {
+    return user;
+  }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+  public void setUser(User user) {
+    this.user = user;
+  }
 
-    public String getPool() {
-        return pool;
-    }
+  public String getPool() {
+    return pool;
+  }
 
-    public void setPool(String pool) {
-        this.pool = pool;
-    }
+  public void setPool(String pool) {
+    this.pool = pool;
+  }
 
-    public int getPoolSize() {
-        return poolSize;
-    }
+  public int getPoolSize() {
+    return poolSize;
+  }
 
-    public void setPoolSize(int poolSize) {
-        this.poolSize = poolSize;
-    }
+  public void setPoolSize(int poolSize) {
+    this.poolSize = poolSize;
+  }
 
-    public int getLength() {
-        return length;
-    }
+  public int getLength() {
+    return length;
+  }
 
-    public void setLength(int length) {
-        this.length = length;
-    }
+  public void setLength(int length) {
+    this.length = length;
+  }
 
-    public String getText() {
-        return text;
-    }
+  public String getText() {
+    return text;
+  }
 
-    public void setText(String text) {
-        this.text = text;
-    }
+  public void setText(String text) {
+    this.text = text;
+  }
 
-    public List<Guess> getGuesses() {
-        return guesses;
-    }
+  public List<Guess> getGuesses() {
+    return guesses;
+  }
 
-    public boolean isSolved() {
-        return guesses
-                .stream()
-                .anyMatch((guess) -> guess.getExactMatches() == length);
-    }
+  public boolean isSolved() {
+    return guesses
+        .stream()
+        .anyMatch((guess) -> guess.getExactMatches() == length);
+  }
 
-    @JsonProperty(value = "text")
-    public String getSecretCode() {
-        return isSolved() ? text : null;
-    }
+  @JsonProperty(value = "text")
+  public String getSecretCode() {
+    return isSolved() ? text : null;
+  }
+
 }
--- src/main/java/edu/cnm/deepdive/service/GameService.java
+++ src/main/java/edu/cnm/deepdive/service/GameService.java
@@ -1,147 +1,156 @@
 package edu.cnm.deepdive.service;
 
+
 import edu.cnm.deepdive.model.dao.GameRepository;
 import edu.cnm.deepdive.model.dao.GuessRepository;
 import edu.cnm.deepdive.model.entity.Game;
 import edu.cnm.deepdive.model.entity.Guess;
 import edu.cnm.deepdive.model.entity.User;
-import org.springframework.stereotype.Service;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
 
-    private final GameRepository gameRepository;
-    private final GuessRepository guessRepository;
-    private final Random rng;
+  private final GameRepository gameRepository;
+  private final GuessRepository guessRepository;
+  private final Random rng;
 
-    public GameService(GameRepository gameRepository, GuessRepository guessRepository, Random rng) {
-        this.gameRepository = gameRepository;
-        this.guessRepository = guessRepository;
-        this.rng = rng;
-    }
+  @Autowired
+  public GameService(
+      GameRepository gameRepository, GuessRepository guessRepository, Random rng) {
+    this.gameRepository = gameRepository;
+    this.guessRepository = guessRepository;
+    this.rng = rng;
+  }
 
-    public Optional<Game> get(UUID id) {
-        return gameRepository.findByExternalKey(id);
-    }
+  public Optional<Game> get(UUID id) {
+    return gameRepository.findById(id);
+  }
 
-    public Optional<Game> get(UUID id, User user) {
-        return gameRepository.findByExternalKeyAndUser(id, user);
-    }
+  public Optional<Game> get(UUID key, User user) {
+    return gameRepository.findByExternalKeyAndUser(key, user);
+  }
 
-    public void delete(UUID id) {
-        gameRepository.deleteById(id);
-    }
+  public void delete(UUID id) {
+    gameRepository.deleteById(id);
+  }
 
-    public void delete(UUID key, User user) {
-        gameRepository
-                .findByExternalKeyAndUser(key, user)
-                .ifPresent(gameRepository::delete);
-    }
+  public void delete(UUID key, User user) {
+    gameRepository
+        .findByExternalKeyAndUser(key, user)
+        .ifPresent(gameRepository::delete);
+  }
 
-    public Game startGame(Game game, User user) {
-        int[] codePoints = preprocess(game.getPool());
-        String code = generateCode(codePoints, game.getLength());
-        game.setUser(user);
-        game.setPool(new String(codePoints, 0, codePoints.length));
-        game.setText(code);
-        game.setPoolSize(codePoints.length);
-        return gameRepository.save(game);
-    }
+  public Game startGame(Game game, User user) {
+    int[] codePoints = preprocess(game.getPool());
+    String code = generateCode(codePoints, game.getLength());
+    game.setUser(user);
+    game.setPool(new String(codePoints, 0, codePoints.length));
+    game.setText(code);
+    game.setPoolSize(codePoints.length);
+    return gameRepository.save(game);
+  }
 
-    public Guess processGuess(UUID gameKey, Guess guess, User user)
-            throws IllegalArgumentException, IllegalStateException, NoSuchElementException {
-        return gameRepository
-                .findByExternalKeyAndUser(gameKey, user)
-                .map((game) -> {
-                    int[] guessCodePoints = preprocessGuess(guess, game);
-                    int[] codeCodePoints = getCodePoints(game.getText());
-                    computeMatches(guess, guessCodePoints, codeCodePoints);
-                    guess.setGame(game);
-                    return guessRepository.save(guess);
-                })
-                .orElseThrow();
-    }
+  public Guess processGuess(UUID gameKey, Guess guess, User user)
+      throws IllegalArgumentException, IllegalStateException, NoSuchElementException {
+    return gameRepository
+        .findByExternalKeyAndUser(gameKey, user)
+        .map((game) -> {
+          int[] guessCodePoints = preprocessGuess(guess, game);
+          int[] codeCodePoints = getCodePoints(game.getText());
+          computeMatches(guess, guessCodePoints, codeCodePoints);
+          guess.setGame(game);
+          return guessRepository.save(guess);
+        })
+        .orElseThrow();
+  }
 
-    private void computeMatches(Guess guess, int[] guessCodePoints, int[] codeCodePoints) {
-        int exactMatches = 0;
-        Map<Integer, Integer> guessCodePointCounts = new HashMap<>();
-        Map<Integer, Integer> codeCodePointCounts = new HashMap<>();
-        for (int i = 0; i < guessCodePoints.length; i++) {
-            if (guessCodePoints[i] == codeCodePoints[i]) {
-                exactMatches++;
-            } else {
-                guessCodePointCounts.put(guessCodePoints[i],
-                        1 + guessCodePointCounts.getOrDefault(guessCodePoints[i], 0));
-                codeCodePointCounts.put(codeCodePoints[i],
-                        1 + codeCodePointCounts.getOrDefault(codeCodePoints[i], 0));
-            }
-            guess.setExactMatches(exactMatches);
-            int nearMatches = guessCodePointCounts
-                    .entrySet()
-                    .stream()
-                    .mapToInt((entry) ->
-                            Math.min(entry.getValue(), codeCodePointCounts.getOrDefault(entry.getKey(), 0)))
-                    .sum();
-            guess.setNearMatches(nearMatches);
-        }
+  private void computeMatches(Guess guess, int[] guessCodePoints, int[] codeCodePoints) {
+    int exactMatches = 0;
+    Map<Integer, Integer> guessCodePointCounts = new HashMap<>();
+    Map<Integer, Integer> codeCodePointCounts = new HashMap<>();
+    for (int i = 0; i < guessCodePoints.length; i++) {
+      if (guessCodePoints[i] == codeCodePoints[i]) {
+        exactMatches++;
+      } else {
+        guessCodePointCounts.put(guessCodePoints[i],
+            1 + guessCodePointCounts.getOrDefault(guessCodePoints[i], 0));
+        codeCodePointCounts.put(codeCodePoints[i],
+            1 + codeCodePointCounts.getOrDefault(codeCodePoints[i], 0));
+      }
     }
+    guess.setExactMatches(exactMatches);
+    int nearMatches = guessCodePointCounts
+        .entrySet()
+        .stream()
+        .mapToInt((entry) ->
+            Math.min(entry.getValue(), codeCodePointCounts.getOrDefault(entry.getKey(), 0)))
+        .sum();
+    guess.setNearMatches(nearMatches);
+  }
 
-    private int[] preprocessGuess(Guess guess, Game game) {
-        if (game.isSolved()) {
-            throw new IllegalStateException("Game is already solved.");
-        }
-        int[] guessCodePoints = getCodePoints(guess.getText());
-        if (guessCodePoints.length != game.getLength()) {
-            throw new IllegalArgumentException(String.format(
-                    "Guess must have the same length (%d) as the secret code.", game.getLength()));
-        }
-        Set<Integer> poolCodePoints = game
-                .getPool()
-                .codePoints()
-                .boxed()
-                .collect(Collectors.toSet());
-        if (IntStream
-                .of(guessCodePoints)
-                .anyMatch((codePoint) -> !poolCodePoints.contains(codePoint))) {
-            throw new IllegalArgumentException(String.format("Guess may only contain the characters in the pool \"%s\"", game.getPool()));
-        }
-        return guessCodePoints;
+  private int[] preprocessGuess(Guess guess, Game game) {
+    if (game.isSolved()) {
+      throw new IllegalStateException("Game is already solved.");
     }
+    int[] guessCodePoints = getCodePoints(guess.getText());
+    if (guessCodePoints.length != game.getLength()) {
+      throw new IllegalArgumentException(String.format(
+          "Guess must have the same length (%d) as the secret code.", game.getLength()));
+    }
+    Set<Integer> poolCodePoints = game
+        .getPool()
+        .codePoints()
+        .boxed()
+        .collect(Collectors.toSet());
+    if (IntStream
+        .of(guessCodePoints)
+        .anyMatch((codePoint) -> !poolCodePoints.contains(codePoint))) {
+      throw new IllegalArgumentException(String.format(
+          "Guess may only contain the characters in the pool \"%s\"", game.getPool()));
+    }
+    return guessCodePoints;
+  }
 
-    private int[] preprocess(String pool) {
-        return pool
-                .codePoints()
-                .peek((codePoint) -> {
-                    if (!Character.isDefined(codePoint)) {
-                        throw new IllegalArgumentException(
-                                String.format("Undefined character in pool: %d", codePoint));
-                    } else if (Character.isWhitespace(codePoint)) {
-                        throw new IllegalArgumentException(
-                                String.format("Whitespace character in pool: %d", codePoint));
-                    }
-                })
-                .sorted()
-                .distinct()
-                .toArray();
-    }
+  private int[] preprocess(String pool) {
+    return pool
+        .codePoints()
+        .peek((codePoint) -> {
+          if (!Character.isDefined(codePoint)) {
+            throw new IllegalArgumentException(
+                String.format("Undefined character in pool: %d", codePoint));
+          } else if (Character.isWhitespace(codePoint)) {
+            throw new IllegalArgumentException(
+                String.format("Whitespace character in pool: %d", codePoint));
+          }
+        })
+        .sorted()
+        .distinct()
+        .toArray();
+  }
 
-    private String generateCode(int[] codePoints, int length) {
-        int[] code = IntStream
-                .generate(() -> codePoints[rng.nextInt(codePoints.length)])
-                .limit(length)
-                .toArray();
-        return new String(code, 0, codePoints.length);
-    }
+  private String generateCode(int[] codePoints, int length) {
+    int[] code = IntStream
+        .generate(() -> codePoints[rng.nextInt(codePoints.length)])
+        .limit(length)
+        .toArray();
+    return new String(code, 0, code.length);
+  }
 
-    private int[] getCodePoints(String source) {
-        return source
-                .codePoints()
-                .toArray();
-    }
+  private int[] getCodePoints(String source) {
+    return source
+        .codePoints()
+        .toArray();
+  }
 
 }
